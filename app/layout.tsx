bf00c3e47222fccd02ef6cc0d351214d453ca826
Maximo Ozonas <maxiozonas10@gmail.com>
2025-03-30 16:03:17 -0300

    Fix en metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Madryn Empleos - Ofertas laborales en Puerto Madryn",
     description: "Explora las mejores ofertas laborales en Puerto Madryn. Encuentra empleos actualizados y oportunidades en Madryn Empleos.",
-    url: "https://madrynempleos.com",
+    url: "https://www.madrynempleos.com",
     siteName: "Madryn Empleos",
     images: [
       {
-        url: "https://madrynempleos.com/lib/logoPage.png",
+        url: "https://www.madrynempleos.com/lib/logoPage.png",
         width: 800,
         height: 600,
         alt: "Madryn Empleos",
@@ -36,6 +36,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <meta name="google-site-verification" content="TU_CÓDIGO_AQUÍ" />
+      </head>
       <body className={inter.className}>
         <ClientProviders>{children}</ClientProviders>
       </body>
