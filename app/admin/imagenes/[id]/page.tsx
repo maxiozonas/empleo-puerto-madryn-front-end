62abab5eafe5ca1d760321b44edc631d4610984d
Juan Ignacio <juanirmariani@gmail.com>
2025-05-07 15:05:35 -0300

    Nuevas funciones

---
+++ app/admin/imagenes/[id]/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { useCategorias, useCategoriaImages } from "@/lib/hooks/useCategorias";
+import Loader from "@/components/ui/loader";
+import Error from "@/components/ui/error";
+import VolverButton from "@/components/ui/volver";
+import Link from "next/link"; 
+import { CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
+import { useAuthCheck } from "@/lib/hooks/useAuthCheck";
+import { useRouter } from "next/navigation"; 
+import { useEffect } from "react";
+
+export default function CategoryPage() {
+  const { data: session, status } = useSession();
+  const { id } = useParams();
+  const { data: categories } = useCategorias();
+  const { data: images } = useCategoriaImages(id as string, session?.backendToken as string);
+  const router = useRouter();
+
+  useAuthCheck();
+
+  useEffect(() => {
+    console.log("Session status:", status);
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status, router]);
+
+  if (status === "unauthenticated") {
+    router.push("/login");
+    return null;
+  }
+
+  if (
+    status === "authenticated" &&
+    !process.env.NEXT_PUBLIC_ADMIN_EMAILS?.split(",").includes(session?.user.email as string)
+  ) {
+    router.push("/");
+    return null;
+  }
+
+  const categoryName = categories?.find((cat) => cat.id === id)?.nombre || "Categoría";
+
+  return (
+    <div className="container min-h-screen mx-auto py-6 px-4">
+      <VolverButton />
+      <Button
+        variant="outline"
+        className="absolute right-8 border-primary text-primary hover:text-primary hover:bg-primary/10 border-2 border-primary/30 rounded-md transition-all duration-300 hover:shadow-lg hover:scale-105 transform"
+        onClick={() => router.push(`/admin/imagenes/${id}/agregar`)}
+      >
+        Agregar imagen
+      </Button>
+      <div className="text-center mb-10">
+        <h1 className="text-3xl font-bold text-primary text-center mb-2 uppercase">{categoryName}</h1>
+        <p className="text-muted-foreground text-center mb-2">Imágenes de {categoryName}</p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {images?.map((imagen, index) => (
+            <Link key={index} href={`/admin/imagenes/${id}/${imagen}`}>
+              <CardContent className="p-6 flex flex-col items-center justify-center text-center h-full hover:bg-gray-100 transition-colors">
+                {imagen}
+              </CardContent>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/admin/imagenes/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect,useState } from "react";
+import { Anchor, BicepsFlexed, Book, BookUser, Briefcase, Building, ChartArea, CircleDollarSign, Coffee, Factory, Fish, Hammer, Handshake, HeartPulse, Laptop, Link, Loader2, Scissors, SquareChartGantt, Truck, Warehouse, Waypoints, Wrench } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useAuthCheck } from "@/lib/hooks/useAuthCheck";
+import { useCategorias } from "@/lib/hooks/useCategorias";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import VolverButton from "@/components/ui/volver";
+
+export default function AdminImages() {
+    const { data: session, status } = useSession();
+    const { data: categorias, isLoading: categoriasLoading } = useCategorias();
+    const [images, setImages] = useState([]);
+    const router = useRouter();
+    
+    useAuthCheck();
+    
+    useEffect(() => {
+        if (status === "unauthenticated") {
+        router.push("/login");
+        }
+    }, [status, router]);
+    
+
+    if (status === "unauthenticated") {
+        router.push("/login");
+        return null;
+      }
+    
+      if (status === "authenticated" && !process.env.NEXT_PUBLIC_ADMIN_EMAILS?.split(",").includes(session?.user.email as string)) {
+        router.push("/");
+         return null;
+    }
+
+    const categoriasIcons: { [key: string]: React.ReactNode } = {
+        "Turismo": <Anchor className="h-6 w-6" />,
+        "Ventas": <CircleDollarSign className="h-6 w-6" />,
+        "Pesca": <Fish className="h-6 w-6" />,
+        "Marketing": <Briefcase className="h-6 w-6" />,
+        "Tecnología": <Laptop className="h-6 w-6" />,
+        "Deporte": <BicepsFlexed className="h-6 w-6" />,
+        "Ingeniería": <Factory className="h-6 w-6" />,
+        "Diseño": <SquareChartGantt className="h-6 w-6" />,
+        "Producción": <Building className="h-6 w-6" />,
+        "Logística": <Warehouse className="h-6 w-6" />,
+        "Atención al Cliente": <BookUser className="h-6 w-6" />,
+        "Finanzas": <ChartArea className="h-6 w-6" />,
+        "Administración": <Waypoints className="h-6 w-6" />,
+        "Recursos Humanos": <Handshake className="h-6 w-6" />,
+        "Educación": <Book className="h-6 w-6" />,
+        "Salud": <HeartPulse className="h-6 w-6" />,
+        "Construcción": <Hammer className="h-6 w-6" />,
+        "Servicios": <Wrench className="h-6 w-6" />,
+        "Chofer": <Truck className="h-6 w-6" />,
+        "Gastronomía": <Coffee className="h-6 w-6" />,
+        "Comercio": <Building className="h-6 w-6" />,
+        "Industria": <Factory className="h-6 w-6" />,
+        "Belleza": <Scissors className="h-6 w-6" />,
+        "Otros": <Anchor className="h-6 w-6" />,
+      };
+    
+      if (status === "loading"  || categoriasLoading) {
+        return (
+          <div className="min-h-screen flex flex-col justify-center items-center py-8">
+            <Loader2 className="h-8 w-8 animate-spin mx-auto text-primary" />
+            <p className="mt-2 text-muted-foreground">Cargando...</p>
+          </div>
+        );
+      }
+
+    return (
+        <div className="mb-8">
+            <VolverButton />
+            <h1 className="text-3xl font-bold text-center text-primary">Gestionar Imágenes</h1>
+            <p className="text-center text-muted-foreground">
+                Revisa y gestiona las imágenes de las categorías de la app.
+            </p>
+            {categorias?.map((categoria) => (
+                <Card
+                    key={categoria.id}
+                    className={cn(
+                    "group relative flex flex-col overflow-hidden transition-all duration-300",
+                    "hover:shadow-xl hover:-translate-y-2 hover:bg-primary/5 border-secondary/30",
+                    "bg-gradient-to-b from-white to-secondary/10 min-w-[280px]"
+                    )}
+                >
+                    <a href={`/admin/imagenes/${categoria.id}`}>
+                        <CardContent className="p-6 flex flex-col items-center justify-center text-center h-full">
+                            <div className="mb-4 rounded-full p-3 group-hover:text-primary">
+                            {categoriasIcons[categoria.nombre] || <Briefcase className="h-6 w-6 text-blue-600" />}
+                            </div>
+                            <CardTitle className="text-lg font-semibold group-hover:text-primary text-foreground mb-2">
+                            {categoria.nombre}
+                            </CardTitle>                
+                        </CardContent>
+                    </a>
+                </Card>
+            ))}
+        </div>
+    );
+}
